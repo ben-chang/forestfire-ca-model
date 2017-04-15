7d2b5697368326164afe3b1c4a60aeac32b4e926
Janos Matyas Boros <avatarokwow@gmail.com>
2017-04-15 02:47:08 +0100

    move refresh method from LegendView and MapView subclasses to View superclass

--- model/views.py
+++ model/views.py
@@ -1,35 +1,37 @@
 from Tkinter import Canvas
-import abc
+from abc import ABCMeta, abstractmethod
 
 
 class View(Canvas):
 
-    __metaclass__ = abc.ABCMeta
+    __metaclass__ = ABCMeta
 
     _FUEL_ACTIVE = 0
     _TERRAIN_ACTIVE = 1
     _IGNITIONS_ACTIVE = 2
     _SIMULATION_ACTIVE = 3
+    _N_CLASSES = 10
 
     DEFAULT_FUEL_COLOR_MAP = {
-        0: (0, 0, 0),
-        1: (0, 0, 0),
-        2: (0, 0, 0),
-        3: (0, 0, 0),
-        4: (0, 0, 0),
-        5: (0, 0, 0),
-        6: (0, 0, 0),
-        7: (0, 0, 0),
-        8: (0, 0, 0),
-        9: (0, 0, 0),
-        10: (0, 0, 0)
-    }
+                              0: (0, 0, 0),
+                              1: (0, 0, 0),
+                              2: (0, 0, 0),
+                              3: (0, 0, 0),
+                              4: (0, 0, 0),
+                              5: (0, 0, 0),
+                              6: (0, 0, 0),
+                              7: (0, 0, 0),
+                              8: (0, 0, 0),
+                              9: (0, 0, 0),
+                              10: (0, 0, 0)
+                            }
 
-    def __init__(self, master, width, height,
+    def __init__(self, master, width, height, n_classes=_N_CLASSES,
                  fuel_color_map=DEFAULT_FUEL_COLOR_MAP):
         Canvas.__init__(self, master=master, width=width, height=height)
         self.height = height
         self.width = width
+        self.n_classes = n_classes
         self.fuel_color_map = fuel_color_map
         self.active_part = MapView._FUEL_ACTIVE
         self.is_view_changed = False
@@ -37,53 +39,70 @@ class View(Canvas):
         self.terrain_model = None
         self.ignition_model = None
 
-    @abc.abstractmethod
+    @abstractmethod
     def add_fuel_model(self, fuel_model):
         pass
 
-    @abc.abstractmethod
+    @abstractmethod
     def add_terrain_model(self, terrain_model):
         pass
 
-    @abc.abstractmethod
+    @abstractmethod
     def add_ignition_model(self, ignition_model):
         pass
 
-    @abc.abstractmethod
+    @abstractmethod
     def activate_fuel_view(self):
         pass
 
-    @abc.abstractmethod
+    @abstractmethod
     def activate_terrain_view(self):
         pass
 
-    @abc.abstractmethod
+    @abstractmethod
     def activate_ignition_view(self):
         pass
 
-    @abc.abstractmethod
-    def refresh_canvas(self):
+    @abstractmethod
+    def activate_simulation_view(self):
         pass
 
-    @abc.abstractmethod
+    def refresh(self):
+        if self.active_part == MapView._SIMULATION_ACTIVE:
+            self._draw_simulation_view()
+        else:
+            if self.is_view_changed:
+                if self.active_part == View._FUEL_ACTIVE:
+                    self._draw_fuel_view()
+                elif self.active_part == View._TERRAIN_ACTIVE:
+                    self._draw_terrain_view()
+                else:
+                    self._draw_ignition_view()
+        self.is_view_changed = False
+
+    @abstractmethod
     def _draw_fuel_view(self):
         pass
 
-    @abc.abstractmethod
+    @abstractmethod
     def _draw_terrain_view(self):
         pass
 
-    @abc.abstractmethod
+    @abstractmethod
     def _draw_ignition_view(self):
+        pass
+
+    @abstractmethod
+    def _draw_simulation_view(self):
         pass
 
 
 class MapView(View):
 
-    def __init__(self, master, width, height, legend_view,
+    def __init__(self, master, width, height, legend_view, n_classes=View._N_CLASSES,
                  fuel_color_map=View.DEFAULT_FUEL_COLOR_MAP,):
         View.__init__(self, master=master, width=width, height=height,
-                      fuel_color_map=fuel_color_map)
+                      n_classes=n_classes, fuel_color_map=fuel_color_map)
         self.legend_view = legend_view
 
     @override
@@ -116,25 +135,9 @@ class MapView(View):
         self.active_part = MapView._IGNITIONS_ACTIVE
         self.legend_view.activate_ignition_view()
 
+    @override
     def activate_simulation_view(self):
         self.active_part = MapView._SIMULATION_ACTIVE
-
-    @override
-    def refresh_canvas(self):
-        if self.active_part == MapView._SIMULATION_ACTIVE:
-            self.__draw_simulation_view()
-        else:
-            if self.is_view_changed:
-                if self.active_part == View._FUEL_ACTIVE:
-                    self._draw_fuel_view()
-                elif self.active_part == View._TERRAIN_ACTIVE:
-                    self._draw_terrain_view()
-                else:
-                    self._draw_ignition_view()
-        self.is_view_changed = False
-
-    def __draw_simulation_view(self):
-        pass
 
     @override
     def _draw_fuel_view(self):
@@ -148,13 +151,17 @@ class MapView(View):
     def _draw_ignition_view(self):
         pass
 
+    @override
+    def _draw_simulation_view(self):
+        pass
+
 
 class LegendView(View):
 
-    def __init__(self, master, width, height,
+    def __init__(self, master, width, height, n_classes=View._N_CLASSES,
                  fuel_color_map=View.DEFAULT_FUEL_COLOR_MAP):
         View.__init__(self, master=master, width=width, height=height,
-                      fuel_color_map=fuel_color_map)
+                      n_classes=n_classes, fuel_color_map=fuel_color_map)
 
     @override
     def add_fuel_model(self, fuel_model):
@@ -181,15 +188,8 @@ class LegendView(View):
         self.active_part = View._IGNITIONS_ACTIVE
 
     @override
-    def refresh_canvas(self):
-        if self.is_view_changed:
-            if self.active_part == View._FUEL_ACTIVE:
-                self._draw_fuel_view()
-            elif self.active_part == View._TERRAIN_ACTIVE:
-                self._draw_terrain_view()
-            else:
-                self._draw_ignition_view()
-        self.is_view_changed = False
+    def activate_simulation_view(self):
+        self.active_part = View._SIMULATION_ACTIVE
 
     @override
     def _draw_fuel_view(self):
@@ -201,6 +201,10 @@ class LegendView(View):
 
     @override
     def _draw_ignition_view(self):
+        pass
+
+    @override
+    def _draw_simulation_view(self):
         pass
 
 
